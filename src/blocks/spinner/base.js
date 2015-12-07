20987653b8fc1eff149c32bcbab32b640092b465
Kasen Lam <kasenlam@gmail.com>
2015-12-07 11:40:15 -0600

    Adding singleton method to BaseSpinner

--- src/blocks/spinner/base.js
+++ src/blocks/spinner/base.js
@@ -27,6 +27,21 @@ define( [
 
     });
 
+    BaseSpinner.classMembers({
+        createStandaloneInstance : function(SpinnerClass, $insertionPoint) {
+            var spinnerInst = new SpinnerClass({
+                visible : false
+            });
+            spinnerInst.isRootComponent = true;
+            var promise = spinnerInst.load($insertionPoint).then(function(){
+                return spinnerInst;
+            });
+
+            return promise;
+        }
+    });
+
+
     // Exporting module
     return BaseSpinner;
 
