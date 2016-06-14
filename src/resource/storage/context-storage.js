37849e8cbf76f5f894857b9c1a80e80c5e4a68a0
Kasen Lam <kasenlam@gmail.com>
2016-06-14 11:38:21 -0600

    issue#41: Optimize the Context Loader for same payload in components

--- src/resource/storage/context-storage.js
+++ src/resource/storage/context-storage.js
@@ -24,6 +24,11 @@ define([
     'use strict';
 
     /**
+     * Constants
+     */
+    var KEYS_SEPARATOR = ',';
+
+    /**
      * App config manager in the client side
      * Contains application settings coming from the server side
      */
@@ -41,7 +46,7 @@ define([
          *         XYZ : RESPONSE OBJECT
          *     },
          *     handlerAlt : {
-         *         XYZ : RESPONSE OBJECT
+         *         ABC,XYZ : RESPONSE OBJECT
          *     }
          * }
          * @param  {string} groupName Optional. Name of the group
@@ -53,7 +58,8 @@ define([
             // Copy all the new vars to the current instance dictionary
             for (var varKey in vars) {
                 if (vars.hasOwnProperty(varKey)) {
-                    varsDict[varKey] = $.extend(varsDict[varKey], vars[varKey]);
+                    var expandedKeys = this._expandKeys(vars[varKey]);
+                    varsDict[varKey] = $.extend(varsDict[varKey], expandedKeys);
                 }
             }
         },
@@ -74,6 +80,55 @@ define([
 
             var data = handlerData[component.id];
             return data;
+        },
+        /**
+         * Given an object with compressed keys, this method will expand it to individual
+         * keys, each one with the same common payload.
+         * For example:
+         * Given the following object:
+         *
+               "patient_today_appointment": {
+                    "27cb7ebc-86a4-46b7-892a-9b7170e4ed74,9edfb869-1cd3-4b2d-88fc-18ed380f8b1a": {
+                        "id_event": null,
+                        "id_patient_checkup_record": null,
+                        "label": "iniciar consulta"
+                    }
+                }
+         *  The final result should be:
+         *
+                "patient_today_appointment": {
+                    "27cb7ebc-86a4-46b7-892a-9b7170e4ed74": {
+                        "id_event": null,
+                        "id_patient_checkup_record": null,
+                        "label": "iniciar consulta"
+                    },
+                    "9edfb869-1cd3-4b2d-88fc-18ed380f8b1a": {
+                        "id_event": null,
+                        "id_patient_checkup_record": null,
+                        "label": "iniciar consulta"
+                    },
+                }
+         * @return Object Expanded object
+         */
+        _expandKeys : function(collapsedObject) {
+            if (!collapsedObject) {
+                return collapsedObject;
+            }
+
+            var expandedKeys = {};
+            $.each(collapsedObject, function(keysString, value) {
+                var keys = keysString.split(KEYS_SEPARATOR);
+                if (keys.length > 1) {
+                    $.each(keys, function(index, key) {
+                        expandedKeys[key] = value;
+                    });
+                } else {
+                    expandedKeys[keysString] = value;
+                }
+            });
+
+            return expandedKeys;
+
         }
     });
 
